3cb19950273f0d14f62cf79a32be66a45fdef50e
dania-c <workingdania@hotmail.com>
2022-03-14 14:28:08 +0200

    intro.jsx with ityped js library for played text

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,46 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    // const myElement = document.querySelector("#myElement");
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: [
+        "Constructor!",
+        "Expert Man!",
+        "Man of word",
+        "brother of Mhamad ",
+      ],
+    });
+
+    console.log(textRef);
+  }, []);
+  //   render(){
+  //     return <div id="myElement"></div>
+  // }
   return (
     <div className='intro' id='intro'>
-      intr
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src='assets/hadi1.jpg' alt='hadi' />
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi There, I'm</h2>
+          <h1> Hadi Ahmad </h1>
+          <h3>
+            <span ref={textRef}>.. </span>/ Freelancer
+          </h3>
+          <a href='#portfolio'>
+            <img src='assets/down.png' alt='f' />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
